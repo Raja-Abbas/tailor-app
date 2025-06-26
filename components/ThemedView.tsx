d8975d8e908e15f0d228b9d8bf106cb003ae7f61
raja-abbas <abbasaffandi2002@gmail.com>
2025-06-26 17:44:57 -0300

    updated UI

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -6,7 +6,6 @@ export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
 };
-
 export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
